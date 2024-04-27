b4878584c735731444b1eab690ed3b7a284a5d10
Swapneswarnayak <1705835@kiit.ac.in>
2024-04-28 03:16:08 +0530

    carousel in making

--- src/Components/ItemBody/Mainbody/Mainbody.js
+++ src/Components/ItemBody/Mainbody/Mainbody.js
@@ -1,26 +1,50 @@
-import { Box, Typography } from "@mui/material";
+import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
+import BasicDemo from "../../Slider/Slider";
 
 const Mainbody = () => {
   return (
     <Box
       sx={{
         width: "80%",
-        // border: "2px solid green",
+        border: "2px solid green",
         p: 3,
         fontFamily: "serif",
       }}
     >
-      <Box >
+      <Box sx={{ border: "2px solid black", mb: 2 }}>
         <Typography
           variant="h5"
-          sx={{ fontFamily: "serif", color: "#333333", letterSpacing: 1.2,mb:0.7 }}
+          sx={{
+            fontFamily: "serif",
+            color: "#333333",
+            letterSpacing: 1.2,
+            mb: 0.7,
+          }}
         >
           All European brands
         </Typography>
-        <p style={{ fontFamily: "monospace",fontWeight:500,letterSpacing: 1.2, }}>
+        <p
+          style={{
+            fontFamily: "monospace",
+            fontWeight: 500,
+            letterSpacing: 1.2,
+          }}
+        >
           Shop independent brands from around the world at wholesale prices.
         </p>
+      </Box>
+
+      <Box sx={{ flexGrow: 1, border: "2px solid yellow" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={3} sx={{ border: "2px solid red" }}>
+            <BasicDemo />
+          </Grid>
+          <Grid item xs={4} sx={{ border: "2px solid red" }}>
+            hellow
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
